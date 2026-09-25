3ee65e32bf72b709cea57b4e3b9c941ec4c94658
Smiling <erin.montg.2011@gmail.com>
2026-09-25 16:19:53 -0600

    Motor control subsystems

--- src/main/java/frc/robot/Mechanisms/IndexSub.java
+++ src/main/java/frc/robot/Mechanisms/IndexSub.java
@@ -1,5 +1,55 @@
-package frc.robot.Mechanisms;
+package frc.robot.subsystems;
 
-public class IndexSub {
-    
-}
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+
+  /** Creates a new ExampleSubsystem. */
+
+
+    private public TalonFX motor_rollersFx = new TalonFX(0);
+    private final VelocityVoltage velocityControl = new VelocityVoltage(0.00);
+    private final PositionVoltage position = new PositionVoltage(00);
+
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+    }
