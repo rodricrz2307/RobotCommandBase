41dd055d13f1e63cb247c7b12edeebb3ead33a10
AloGuerrero <154291966+AloGuerrero@users.noreply.github.com>
2026-09-25 16:47:56 -0600

    Update IndexCommands.java

--- src/main/java/frc/robot/Commands/IndexCommands.java
+++ src/main/java/frc/robot/Commands/IndexCommands.java
@@ -1,5 +1,32 @@
-package frc.robot.Commands;
+package frc.robot.commands;
 
-public class IndexCommands {
-    
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class IndexCommands extends Command {
+
+    private final IndexSub indexSub;
+    private final double speed;
+
+  public IndexCommands(IndexSub indexSub, double speed){
+    this.indexSub = indexSub;
+    this.speed = speed;
+    addRequirements(indexSub);
+
+  }
+
+  @Override
+  public void eat() {
+
+    indexSub.setRollerSpeed(3);
+
+  }
+
+  @Override
+  public void desatasco(){
+    indexSub.setRollerSpeed(-3);
+  }
+
+
 }
+
