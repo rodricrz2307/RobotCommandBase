4b1c4f73e4a1bfc7b2f20754391e570feadc1ba1
M.isaki11 <charizard11100@gmail.com>
2026-09-25 16:23:04 -0600

    Update OutakeCommands.java

--- src/main/java/frc/robot/Commands/OutakeCommands.java
+++ src/main/java/frc/robot/Commands/OutakeCommands.java
@@ -1,5 +1,33 @@
 package frc.robot.Commands;
 
-public class OutakeCommands {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Mechanisms.IntakeSub;
+import frc.robot.Mechanisms.OutakeSub;
+
+
+public class OutakeCommands{
     
+public class OutakeCommands extends Command {
+
+    private final OutakeSub OutakeSub;
+    private final double speed;
+
+   public OutakeCommands (OutakeSub outakeSub, double speed) {
+    this.outakeSub = outakeSub;
+    this.speed = speed;
+    addRequirements(outakeSub);
 }
+
+@Override
+public void execute() {
+    outakeSub.setOutakeSpeed(speed);
+}
+
+@Override
+public void end(boolean interrupted) {
+    outakeSub.stopOutake();
+}
+}
+
+   }
